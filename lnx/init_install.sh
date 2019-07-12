1a36269c580a1a7939b0973bbe1caa467f8cc398
Paulo F <paulof@gmail.com>
2019-07-12 14:20:14 -0300

    improving i3 configuration

---
+++ lnx/init_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install i3 ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg numlockx xautolock scrot xorg xserver-xorg wget unzip wpasupplicant
+sudo apt-get install gnome-screensaver parcellite volti network-manager-gnome xbacklight alsa-utils pulseaudio i3lock microcode.ctl intel-microcode
+cp i3.config ~/.config/i3/config
+cp i3status ~/.config/i3/config
