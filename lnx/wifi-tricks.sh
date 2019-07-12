0618f5088dc1d260cf7f67b84d5ec4794c207be6
Paulo F <paulof@gmail.com>
2019-07-12 18:13:50 -0300

    add wifi tricks

---
+++ lnx/wifi-tricks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Disable 802.11n - https://itsfoss.com/speed-up-slow-wifi-connection-ubuntu/
+#sudo rmmod iwlwif
+#sudo modprobe iwlwifi 11n_disable=1
+
+# Permanent
+#sudo su
+#echo "options iwlwifi 11n_disable=1" >> /etc/modprobe.d/iwlwifi.conf
