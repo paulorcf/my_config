747067c2677678f3eae38dd85ce1ca7e99e78c7b
Paulo F <paulof@gmail.com>
2019-07-12 15:11:35 -0300

    adding developer script

---
+++ lnx/init_dev_env.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+sudo apt install -y qutebrowser gstreamer1.0-plugins-{bad,base,good,ugly}
+
+# Initial dev stack 
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libreadline-dev wget
+
+# Python
+sudo apt-get install -y python3-pip libssl-dev libffi-dev python-dev python3-venv
+
+# Java
+sudo apt install -y openjdk-11-jdk
+# SDKMan
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+
+# Java stack
+sdk install maven
+
+# Docker
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+
+# Gcloud
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk google-cloud-sdk-app-engine-java google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-python-extras google-cloud-sdk-pubsub-emulator 
+
+# AWS-CLI
+pip3 install awscli --upgrade --user
+
+# Others
+# sudo update-alternatives --config java
+# sudo update-alternatives --config javac
+
--- lnx/init_install.sh
+++ lnx/init_install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-sudo apt install i3 ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg numlockx xautolock scrot xorg xserver-xorg wget unzip wpasupplicant
+sudo apt install i3 ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg numlockx xautolock scrot xorg xserver-xorg wget unzip wpasupplicant neovim iptables ip6tables
 sudo apt-get install gnome-screensaver parcellite volti network-manager-gnome xbacklight alsa-utils pulseaudio i3lock microcode.ctl intel-microcode
 cp i3.config ~/.config/i3/config
 cp i3status ~/.config/i3/config
+
+# Retro term
+echo "deb http://ppa.launchpad.net/vantuz/cool-retro-term/ubuntu bionic main" | sudo tee -a /etc/apt/source.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BDB05D75
+sudo apt update
+sudo apt install cool-retro-term
+
+# Cool font
+wget "https://use.fontawesome.com/releases/v5.0.13/fontawesome-free-5.0.13.zip"
+unzip fontawesome-free-5.0.13.zip
+sudo cp fontawesome-free-5.0.13/use-on-desktop/* /usr/local/share/fonts/
+fc-cache -f -v
+#the next command help see the name to use
+#here Font Awesome 5 Free
+fc-list | grep -i "awe"
+
+# Security
+sudo cp iptables/00-iptable-flush /usr/lib/networkd-dispatcher/routable.d/
+sudo cp iptables/50-iptable-mdns /usr/lib/networkd-dispatcher/routable.d/
+sudo chmod +x /usr/lib/networkd-dispatcher/routable.d/00-iptable-flush
+sudo chmod +x /usr/lib/networkd-dispatcher/routable.d/50-iptable-mdns
+
+# My init profile
+cp xinitrc ~/.xinitrc
